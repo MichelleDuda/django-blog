eb78acc425299de63e8af299bb8a23397424a17a
Michelle Kelly <jttsaz91@students.codeinstitute.net>
2025-01-06 07:42:13 +0000

    Change debug setting for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-michelleduda-djangoblog-qb8uzv9lbr5.ws.codeinstitute-ide.net', '.herokuapp.com']
 
